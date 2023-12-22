0b337010170c08b3081c305c1c3277c3a7f7d409
Bhuvnesh bansal <77397035+bhuvnesh179@users.noreply.github.com>
2023-12-22 15:35:28 +0000

    give functionality to toggle button

--- toggle/toggle.js
+++ toggle/toggle.js
@@ -1,1 +1,20 @@
-console.log("config done");
+var toggled = false;
+var bodyTag = document.getElementsByTagName("body");
+var circle= document.getElementById("circle");
+var hTag = document.getElementsByTagName("h1");
+
+document.getElementById("toggle").onclick = function(){
+    if(!toggled){
+        bodyTag[0].style.backgroundColor = 'black';
+        hTag[0].style.color = 'white';
+        circle.style.marginLeft = "100px";
+
+        toggled = true;
+    }else{
+        bodyTag[0].style.backgroundColor = 'white';
+        hTag[0].style.color = 'black';
+        circle.style.marginLeft = "1px";
+
+        toggled = false;
+    }
+}
